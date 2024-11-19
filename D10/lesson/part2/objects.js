292c42c6bddbcdcad5171e21a85b98435cca0e36
carjunalcarz <cardona.aljun.csucc@gmail.com>
2024-11-19 16:21:30 +0800

    d10 lessons continue

--- D10/lesson/part2/objects.js
+++ D10/lesson/part2/objects.js
@@ -1,6 +1,21 @@
 class User {
-    
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+  login() {
+    console.log(this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+  }
 }
+
+var userOne = new User("aljun@gmail.com", "Aljun");
+var userTwo = new User("alcarz@gmail.com", "alcarz");
+
+userOne.login();
+userTwo.logout();
 
 // the ''new' keyword
 // - creates a new empty object {}
